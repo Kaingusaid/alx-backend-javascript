c4b4296ebc438a13847d8b0ade08b3426d1f8c36
Kaingusaid <kaingusaid22@gmail.com>
2022-12-19 22:00:14 +0000

    getfullbudget

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,20 @@
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
